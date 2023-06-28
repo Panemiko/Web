90861d93cb5b4226bca99f0e22d5ec442d6027bb
Panem <titoluccao@gmail.com>
2023-06-28 16:58:18 -0300

    feat: update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,14 @@ export default function Home() {
       <Head>
         <title>Surredstone</title>
       </Head>
-      <main className="min-h-screen flex flex-col gap-10 justify-center items-center bg-gradient-to-br origin-top-left from-violet-900 shadow-2xl shadow-white to-violet-800">
-        <Image src={logoAsset} className="w-1/2" alt="Logo do Surredstone" />
+      <main className="min-h-screen w-screen flex flex-col gap-24 md:gap-10 justify-center items-center bg-gradient-to-br from-zinc-900 shadow-2xl shadow-white to-violet-950">
+        <Image
+          src={logoAsset}
+          className="w-[stretch] mx-6 md:w-1/2"
+          alt="Logo do Surredstone"
+        />
         <nav>
-          <ul className="flex gap-12">
+          <ul className="flex text-center md:flex-row md:gap-16 flex-col gap-4">
             <li>
               <Link
                 className="text-2xl text-violet-300 hover:text-violet-100 font-bold uppercase transition-colors"
